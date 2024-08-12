57a1e60138b9f566c8bffb1583545742588421ee
RyanRafaelA <ryrafael461@gmail.com>
2024-08-12 10:06:08 -0300

    Implementação da lógica do método CalcularValorDiaria

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -27,18 +27,11 @@ namespace DesafioProjetoHospedagem.Models {
             return Hospedes.Count;
         }
 
-        public decimal CalcularValorDiaria()
-        {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+        public decimal CalcularValorDiaria() {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
+            if (DiasReservados >= 10) {
+                valor -= valor * (decimal)0.1;
             }
 
             return valor;
