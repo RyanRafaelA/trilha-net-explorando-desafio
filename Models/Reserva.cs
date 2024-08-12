f986e706f1c9cb7f3cc627fc0fb35308ed44b34c
RyanRafaelA <ryrafael461@gmail.com>
2024-08-12 09:53:46 -0300

    Implementação da lógica do método CadastrarHospedes

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,30 +1,23 @@
-namespace DesafioProjetoHospedagem.Models
-{
-    public class Reserva
-    {
+namespace DesafioProjetoHospedagem.Models {
+    public class Reserva {
         public List<Pessoa> Hospedes { get; set; }
         public Suite Suite { get; set; }
         public int DiasReservados { get; set; }
 
         public Reserva() { }
 
-        public Reserva(int diasReservados)
-        {
+        public Reserva(int diasReservados) {
             DiasReservados = diasReservados;
         }
 
-        public void CadastrarHospedes(List<Pessoa> hospedes)
-        {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
+        public void CadastrarHospedes(List<Pessoa> hospedes) {
+            if (hospedes.Count <= Suite.Capacidade) {
                 Hospedes = hospedes;
             }
-            else
-            {
+            else {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // *IMPLEMENTE AQUI//
+                throw new Exception("A quantidade de hospedes excedeu a capaciadade do quarto.");
             }
         }
 
